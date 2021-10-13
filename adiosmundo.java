c518c467fd195d919662c1c351a51cda77a5ca41
Rubén <rmartinezdominguez@danielcastelao.org>
2021-10-13 14:43:31 +0200

    colorinchos

--- adiosmundo.java
+++ adiosmundo.java
@@ -1,6 +1,8 @@
 // pongo esto por poner algo equisde
 public class Adiosmundo {
     public static void main(String[] args) {
+        String naranjito = "\032[32m";
+        String azuloide = "\033[34m";
         System.out.println("Arrivederci mundo!");
         System.out.println("Chorrada añadida en la 1.1");
     }
